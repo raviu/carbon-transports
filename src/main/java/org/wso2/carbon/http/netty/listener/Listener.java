e2023507410469ac462b88d6500cb0f8ca809f30
RaivU <ravi.undupitiya@gmail.com>
2015-08-04 12:10:49 +0530

    Clean up import

--- src/main/java/org/wso2/carbon/http/netty/listener/Listener.java
+++ src/main/java/org/wso2/carbon/http/netty/listener/Listener.java
@@ -25,7 +25,6 @@ import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import org.apache.log4j.Logger;
-import org.wso2.carbon.api.Engine;
 import org.wso2.carbon.http.netty.internal.NettyTransportDataHolder;
 
 import java.util.List;
